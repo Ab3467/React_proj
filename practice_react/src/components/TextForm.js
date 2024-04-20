a1d53d8aff00681bf4428406f008114039dcd978
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-20 15:40:42 +0500

    adding uppercase function

--- practice_react/src/components/TextForm.js
+++ practice_react/src/components/TextForm.js
@@ -2,16 +2,18 @@ import React,{useState} from 'react'
 
 export default function TextForm(props) {
     const handleClick=()=> {
-        console.log("Uppercase is clicked");
+        setText(text.toUpperCase());
     }
-    
-    const {text,setText} = useState("Enter Text here")
+    const handleChange=(e)=>{
+        setText(e.target.value)
+    }
+    const [text,setText]= useState("Enter Text here")
    
   return (
     <div style={{ textAlign: 'center' }}>
       <h1 className='mt-5'>{props.heading}</h1>
       <div className="ml-5 mt-5">
-        <textarea name="form-control" value={text} id="my-box" rows="20" cols="100"></textarea>
+        <textarea className="mt-1 ml-1" name="form-control" value={text} onChange={handleChange} id="my-box" rows="20" cols="100"></textarea>
       </div>
       <button className='btn btn-primary' onClick={handleClick}>Convert to UpperCase</button>
     </div>
