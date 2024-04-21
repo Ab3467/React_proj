c94f436f5904cd4e49d66a14bfc4b86665f21737
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-21 12:25:21 +0500

    making Navbar and applying theme

--- practice_react/src/App.js
+++ practice_react/src/App.js
@@ -1,11 +1,14 @@
-
+import React,{useState} from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import About from './components/About';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode,setMode]=useState("light")
   return (
     <>
+    <Navbar title="VyroVerse" mode={mode}/>
     <div>
       <TextForm heading="Enter Text Here"/>
     </div>
