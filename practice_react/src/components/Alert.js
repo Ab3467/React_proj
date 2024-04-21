6c845eb0d77113db5376e11c7c820169f9605330
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-21 15:21:49 +0500

    Changes

--- practice_react/src/components/Alert.js
+++ practice_react/src/components/Alert.js
@@ -4,7 +4,7 @@ export default function Alert(props) {
   return (
     <>
       {props.alert && ( // Check if props.alert is not null or undefined
-        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+        <div className={`alert alert-${props.alert?.msg} alert-dismissible fade show`} role="alert">
           <strong>{props.alert?.type}</strong>: {props.alert?.msg}
           <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
