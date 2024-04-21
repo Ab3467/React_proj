bfe65324f778a4570b1dc9969d769bfd6183d527
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-21 14:48:12 +0500

    ALert now working

--- practice_react/src/components/Alert.js
+++ practice_react/src/components/Alert.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function Alert(props) {
   return (
-    
-      <>
-   props.alert && <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>{props.alert.type}</strong>: {props.alert.msg}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-      </>
-    
-  )
+    <>
+      {props.alert && ( // Check if props.alert is not null or undefined
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+          <strong>{props.alert?.type}</strong>: {props.alert?.msg}
+          <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      )}
+    </>
+  );
 }
+
